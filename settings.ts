faa25b1b2d80c1d3e8521a3396a8fd3a6d9c68b4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-08-19 03:11:02 +0200

    Added basicAuth example to settings file

--- settings.ts
+++ settings.ts
@@ -3,4 +3,8 @@ import { Settings } from "./src/server/types";
 export default {
   host: "localhost",
   port: 3000,
+  // basicAuth: {
+  //   username: "admin",
+  //   password: "password",
+  // },
 } as Settings;
